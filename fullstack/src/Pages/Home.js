8983c6901b03b29ef35a2fe6cf2bfe9ecb1c7850
CarolineSiqueira <santos.carolinesiqueira@gmail.com>
2021-02-12 13:56:38 -0300

    1 alteração

---
+++ fullstack/src/Pages/Home.js
@@ -0,0 +1,16 @@
+import { Jumbotron, Container } from 'react-bootstrap'
+
+export default function Home(){
+    return(
+        <Jumbotron fluid>
+        <Container>
+          <h1>Seja bem-vindo(a)!</h1>
+          <p>
+          Aqui em nossa loja,programadores tem desconto!
+          </p>
+        </Container>
+      </Jumbotron>
+
+      
+    )
+}
